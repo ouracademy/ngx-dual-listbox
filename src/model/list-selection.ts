cdd2b066ac5c196d4c5f69f43c4810b4ad2f771b
ArthurMauricioDelgadillo <this.arthur.email@gmail.com>
2017-09-09 17:45:09 -0500

    feat(model): add list selection

--- src/model/list-selection.ts
+++ src/model/list-selection.ts
@@ -1,4 +1,8 @@
-interface ListSelection {
+export interface ListSelection {
+  selectedItems: any[];
+
+  totalItems: any[];
+
   select(item: any): void;
 
   selectAll(): void;
@@ -6,8 +10,36 @@ interface ListSelection {
   unselect(item: any): void;
 
   isSelected(item: any): boolean;
+}
 
-  selectedItems(): any[];
+export class ListSelectionImpl implements ListSelection {
+  private _selectedItems: any[] = [];
 
-  totalItems(): any[];
+  constructor(public readonly totalItems: any[]) {}
+
+  select(item: any): void {
+    if (!this.isSelected(item)) {
+      this._selectedItems.push(item);
+    }
+  }
+
+  selectAll(): void {
+    this._selectedItems = this.totalItems;
+  }
+
+  unselect(item: any): void {
+    if (!this.isSelected(item)) {
+      throw new Error(`Cannot unselect an item that is not selected`);
+    }
+
+    this._selectedItems = this._selectedItems.filter(e => e !== item);
+  }
+
+  isSelected(item: any): boolean {
+    return !!this._selectedItems.find(e => e === item);
+  }
+
+  get selectedItems(): any[] {
+    return this._selectedItems;
+  }
 }
